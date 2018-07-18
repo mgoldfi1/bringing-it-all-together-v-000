5580d69c9e521f92008e306d40ce465a14fc3c33
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-18 17:25:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,7 +36,8 @@ attr_accessor :name, :breed, :id
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0],result[1],result[2])
+    attributes = {id: result[0], name: result[1], breed: result[2]}
+    Dog.new(attributes)
   end
 
 
