a497f77939df9f21fb71e36da2728dcd25f30e2e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-18 17:17:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,12 +18,12 @@ attr_accessor :name, :breed, :id
    sql = <<-SQL
    INSERT INTO dogs (name, breed) VALUES (?,?)
    SQL
-   if self.id 
+   if self.id
       self.update
-   else 
+   else
    DB[:conn].execute(sql, self.name, self.breed)
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
- end
+  end
 
   end
 
