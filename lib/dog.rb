a8e96cfa92ac69b266b8dc3a3f72bc0818c810ff
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-18 20:29:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,7 @@ attr_accessor :name, :breed, :id
   end
 
     def self.new_from_db(row)
-      dog = Dog.new(row[1,row[2], id = row[0])
+      dog = Dog.new(row[1],row[2], id = row[0])
 
     end
 
